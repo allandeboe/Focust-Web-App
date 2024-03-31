e9b9c57dd1c424533d4fcaf79ea71488b5610beb
Allan DeBoe <allan.m.deboe@gmail.com>
2024-03-31 15:39:21 -0700

    Updated front-end pages to include the site banner logo
    For this commit, I did the following things:
    
    1) Fixed the image size of "focust-banner-ref.svg", so there isn't too much empty space.
    
    2) added "banner.svg" to the front-end server, which is just a modified version of the new "focust-banner-ref.svg" that can be parsed by React's JSX.
    
    3) Updated "Navigation.tsx" to include the new banner, and modified "main_tailwind.css" to accomodate this new change.
    
    4) Updated the two Dockerfiles to fix issues regarding installation of dependencies.

--- focust-front-end/src/components/Navigation.tsx
+++ focust-front-end/src/components/Navigation.tsx
@@ -10,16 +10,22 @@
 
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import Banner from '../banner.svg';
 
 export class Navigation extends Component {
 
     render() {
         return (
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about-us">About Us</Link>
-                <Link to="/users">Users</Link>
-            </nav>
+            <div className="navigation">
+                <div className="text-center block" style={{ margin: "auto", width: "15%" }}>
+                    <img src={Banner} alt="Focust Banner" className="w-full"/>
+                </div>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about-us">About Us</Link>
+                    <Link to="/users">Users</Link>
+                </nav>
+            </div>
         );
     }
 }
