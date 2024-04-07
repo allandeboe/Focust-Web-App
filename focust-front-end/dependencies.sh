904985400bf72281b9bcc604c809c1702a1ee47a
Allan DeBoe <allan.m.deboe@gmail.com>
2024-04-06 22:45:08 -0700

    Created "dependencies.sh"
    For this commit, I have done the following things:
    
    1) Created two "dependencies.sh" bash script files for both the front-end and back-end servers to handle the installation of dependencies and have docker run it instead of manually installing the dependencies itself. This is to allow more flexibility in terms of how to handle installation issues that Docker cannot provide, like using a loop to keep using "apt-get update --fix-missing" and trying to install a given dependency again until it gets installed successfully.
    
    _______
    
    I wanted to add a new dependency for the back-end to allow me to check the health of the back-end server and use it as a means to see if I can go ahead and start up the front-end server, but including the dependency "spring-boot-starter-actuator" ended up causing problems when building the back-end server, and spent the entire week trying to debug it to no success, but I will still keep working on it. Hopefully in a future commit I can add a health check to the back-end server.

---
+++ focust-front-end/dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# dependencies.sh
+#
+# Used in the Dockerfile to properly compile all of
+# the dependencies that the project might have.
+#
+# @author   Allan DeBoe (allan.m.deboe@gmail.com)
+# @date     March 31st, 2024
+
+declare -a dependencies=("nodejs" "npm")
+
+for value in "${dependencies[@]}"
+do
+    echo -e "/r/n( Installing $value ...)"
+    until ( apt-get install $value -y )
+    do 
+        apt-get update --fix-missing
+    done
+done
